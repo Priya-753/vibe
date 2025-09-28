26b70fca1ea9100eb3ff24af1f619066d7ad1b71
Priya-753 <priya61197@gmail.com>
2025-09-27 21:50:53 -0400

    Created a sandbox for code created by agent using E2B

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,15 @@
 import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
-
+import { Sandbox } from "e2b";
+import { getSandbox } from "./utils";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
-
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+        const sandbox = await Sandbox.create("vibe-nextjs-1759023284");
+        return sandbox.sandboxId;
+    });
     const codeAgent = createAgent({
         name: "codeAgent",
         system: "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js and React snippets.",
@@ -17,7 +21,12 @@ export const helloWorld = inngest.createFunction(
       );
       console.log(output);
       
+      const sandboxUrl = await step.run("get-sandbox-url", async () => {
+        const sandbox = await getSandbox(sandboxId);
+        const host = sandbox.getHost(3000);
+        return `http://${host}`;
+      });
 
-    return { output };
+    return { output, sandboxUrl };
   },
 ); 
